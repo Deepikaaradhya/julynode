726b3fd4bab8414a2ccb44e6533cf6df8245e13b
deepikaaradhya <deepikaaradhya62@gmail.com>
2021-12-05 23:20:39 -0800

    update

--- app.js
+++ app.js
@@ -69,24 +69,24 @@ app.get('/filter/:mealType',(req,res) => {
         limit = Number(req.query.limit); 
     }
     var mealType = req.params.mealType;
-    var query = {"mealTypes.mealtype_id":Number(mealType)};
+    var query = {"type.mealtype":mealType};
     if(req.query.cuisine && req.query.lcost && req.query.hcost){
         query={
             $and:[{cost:{$gt:Number(req.query.lcost),$lt:Number(req.query.hcost)}}],
-            "cuisines.cuisine_id":Number(req.query.cuisine),
-            "mealTypes.mealtype_id":Number(mealType)
+            "Cuisine.cuisine":req.query.cuisine,
+            "type.mealtype.mealtype":mealType
         }    
     }
     else if(req.query.cuisine){
-        query = {"mealTypes.mealtype_id":mealType,"cuisines.cuisine_id":Number(req.query.cuisine)}
+        query = {"type.mealtype":mealType,"Cuisine.cuisine":req.query.cuisine}
         //query={"type.mealtype":mealType,"Cuisine.cuisine":{$in:["1","5"]}}
     }
     else if(req.query.lcost && req.query.hcost){
         var lcost = Number(req.query.lcost);
         var hcost = Number(req.query.hcost);
-        query={$and:[{cost:{$gt:lcost,$lt:hcost}}],"mealTypes.mealtype_id":Number(mealType)}
+        query={$and:[{cost:{$gt:lcost,$lt:hcost}}],"type.mealtype":mealType,}
     }
-    db.collection('restaurants').find(query).sort(sort).skip(skip).limit(limit).toArray((err,result)=>{
+    db.collection('restaurents').find(query).sort(sort).skip(skip).limit(limit).toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
     })
@@ -103,17 +103,6 @@ app.get('/quicksearch',(req,res) =>{
 //restaurant Details
 app.get('/details/:id',(req,res) => {
     var id = req.params.id
-    console.log(id)
-    db.collection('restaurants').find({restaurant_id:Number(id)}).toArray((err,result) =>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
-
-//Menu Details
-app.get('/details/:id',(req,res) => {
-    var id = req.params.id
-    console.log(id)
     db.collection('restaurants').find({restaurant_id:Number(id)}).toArray((err,result) =>{
         if(err) throw err;
         res.send(result)
