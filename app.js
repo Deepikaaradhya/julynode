335465db9c1636d939a1b95d27ee2c3e21135bfb
deepikaaradhya <deepikaaradhya62@gmail.com>
2021-12-06 04:01:03 -0800

    upto

--- app.js
+++ app.js
@@ -109,14 +109,21 @@ app.get('/details/:id',(req,res) => {
     })
 })
 
-
-//menu Details
+//menu Details on bases of restaurant
 app.get('/menu/:id',(req,res) => {
     var id = req.params.id
     db.collection('menu').find({restaurant_id:Number(id)}).toArray((err,result) =>{
         if(err) throw err;
         res.send(result)
     })
+})
+
+app.post('/menuItem',(req,res) =>{
+    console.log(req.body)
+    db.collection('menu').find({menu_id:{$in:req.body.ids}}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })    
 })
 
 //place orders 
